d6c973cacfd5847b37ef00798d764b883214fee0
zhoums <310754790@qq.com>
2020-09-16 20:08:39 +0800

    1.0.1 添加

---
+++ src/main/java/com/atguigu/maven/java01.java
@@ -0,0 +1,12 @@
+package com.atguigu.maven;
+
+/**
+ * @author zhoums
+ * @version 1.0
+ * @date 2020/9/16 20:07
+ */
+public class java01 {
+    void add(){
+        System.out.println("添加购物车");
+    }
+}
